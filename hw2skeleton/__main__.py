ba4c093dd84bae1c8019c7678db2da1614e6559a
maurisa saylor <maurisasaylor@maurisas-MBP.ucsf.edu>
2019-02-06 08:51:03 -0800

    formated the active sites into df

--- hw2skeleton/__main__.py
+++ hw2skeleton/__main__.py
@@ -1,6 +1,9 @@
 import sys
 from .io import read_active_sites, write_clustering, write_mult_clusterings
 from .cluster import cluster_by_partitioning, cluster_hierarchically
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
 # Some quick stuff to make sure the program is called correctly
 if len(sys.argv) < 4:
@@ -8,24 +11,6 @@ if len(sys.argv) < 4:
     sys.exit(0)
 
 active_sites = read_active_sites(sys.argv[2])
-
-# active sites contain residues, contain atoms, contain coords
-# print(active_sites) # prints the file name
-# print(active_sites[0]) # prints the first name
-# print(active_sites[0].residues) # print the type and number
-# print(active_sites[0].residues[0]) # print first type and number
-# print(active_sites[0].residues[0].atoms) # print all the atoms
-# print(active_sites[0].residues[0].atoms[0]) # print the first atom
-# print(active_sites[0].residues[0].atoms[0].coords) # print the x y z coord
-
-for a in active_sites:
-    print(a.residues)
-
-
-
-
-
-
 
 # Choose clustering algorithm
 if sys.argv[1][0:2] == '-P':
@@ -37,3 +22,7 @@ if sys.argv[1][0:2] == '-H':
     print("Clustering using hierarchical method")
     clusterings = cluster_hierarchically(active_sites)
     write_mult_clusterings(sys.argv[3], clusterings)
+
+# make a random grouping to compare against
+# eval clustering alg with davis boulding in
+# plot in pc space for similarity
--- hw2skeleton/cluster.py
+++ hw2skeleton/cluster.py
@@ -1,4 +1,52 @@
 from .utils import Atom, Residue, ActiveSite
+import pandas as pd
+import numpy as np
+
+def format_data(active_sites):
+    # active sites contain residues, contain atoms, contain coords
+
+    # collect the residue, max/min coords for each active site
+    collect_vector_coords = []
+    for s in active_sites:
+        for r in s.residues:
+            collect_bb = [str(s),r.type]
+            collect_a = []
+            for a in r.atoms:
+                collect_a.append(a.coords) # r.atoms[:3] are N, CA, C
+            x_vals = [item[0] for item in collect_a]
+            y_vals = [item[1] for item in collect_a]
+            z_vals = [item[2] for item in collect_a]
+            collect_vals = [max(x_vals),max(y_vals),max(z_vals),min(x_vals),min(y_vals),min(z_vals)]
+            collect_bb.append(collect_vals)
+            collect_vector_coords.append(collect_bb)
+
+    # collect the active site, residue, and max/min coords into df
+    pd_backbone = pd.DataFrame(collect_vector_coords,columns=['activesite','aminoacid','mmcoords'])
+    pd_backbone[['x_max','y_max','z_max','x_min','y_min','z_min']] = pd.DataFrame(pd_backbone.mmcoords.values.tolist(), index= pd_backbone.index)
+    pd_backbone.drop('mmcoords',inplace=True,axis=1)
+
+    # get the distance between the max and min for each residue
+    pd_backbone['x_dist'] = pd_backbone['x_max']-pd_backbone['x_min']
+    pd_backbone['y_dist'] = pd_backbone['y_max']-pd_backbone['y_min']
+    pd_backbone['z_dist'] = pd_backbone['z_max']-pd_backbone['z_min']
+    pd_backbone.drop(['x_max','x_min','y_max','y_min','z_max','z_min'],inplace=True,axis=1)
+
+    # make a vector for the min/max coords
+    pd_backbone['vector'] = pd_backbone[['x_dist','y_dist','z_dist']].values.tolist()
+    pd_backbone.drop(['x_dist','y_dist','z_dist'],inplace=True,axis=1)
+
+    # convert to unit vector
+    pd_backbone['unit_v'] = pd_backbone['vector'].apply(lambda x: x/np.linalg.norm(x))
+    pd_backbone.drop('vector',inplace=True,axis=1)
+
+    # list the residues and max/min coords for each active site
+    group_bb = pd_backbone.groupby(['activesite']).agg(lambda x: list(x))
+    # get angle between each residue
+
+    # return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+    print(pd_backbone.head())
+    return pd_backbone
+
 
 def compute_similarity(site_a, site_b):
     """
@@ -7,10 +55,11 @@ def compute_similarity(site_a, site_b):
     Input: two ActiveSite instances
     Output: the similarity between them (a floating point number)
     """
+    similarity = 0.0 # 0 if no similarity, 1 if the same
+    # cosine similarity
 
-    similarity = 0.0
-
-    # Fill in your code here!
+    # jaccard similarity
+    # kabash algorithm
 
     return similarity
 
@@ -24,7 +73,23 @@ def cluster_by_partitioning(active_sites):
             (this is really a list of clusters, each of which is list of
             ActiveSite instances)
     """
-    # Fill in your code here!
+
+    # len = active_sites.len()
+    # k = 5
+    format_data(active_sites)
+
+
+
+    # compare the similarites
+
+    # pick random starting points for k clusters
+    # assign to nearest cluster centroid
+    # new cluster center by taking the average of the assigned points
+    # recursive 2/3 until non change (bistable? - or cap)
+
+
+
+
 
     return []
 
